77bc0c888bd0b5c07a5fd162d3e3a0b5f03e1fdd
Thaquidheen M A <thaquidheen@gmail.com>
2023-09-06 14:00:19 +0530

    complete

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.authlogin,name='login'),
-    path('registration/',views.register,name='registration'),
+    path('registration/',views.authregister,name='registration'),
     path('forget-password/',views.forgetpassword,name='forgetpassword'),
     path('logout/',views.authlogout,name='logout')
 
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
-
+from django.contrib import messages
+from django.contrib.auth.models import User
 # Create your views here.
 
 def authlogin(request):
@@ -12,20 +13,39 @@ def authlogin(request):
             login(request,user)
             return redirect('profile')
         else:
-            print('Invalid Username or Password')
+            messages.error(request, 'Username or Password Invalid')
 
     return render(request,'authentication/login.html')
 
 def authlogout(request):
     logout(request)
+    messages.success(request, 'Logout Successfully !')
     return redirect('login')
+    
      
 
 
 
 
 
-def register(request):
+def authregister(request):
+    if request.method == 'POST':
+        username= request.POST['name']
+        email= request.POST['email']
+        password= request.POST['password']
+        confirm_password= request.POST['confirm_password']
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username Already Exist')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'Email Already Exist')
+            else:
+                user= User.objects.create_user(username=username,password=password,email=email)
+                user.save()
+                return redirect('login')
+
+        else:
+            messages.error(request, 'Password is not matching')
     return render(request,'authentication/registration.html')
 
 def forgetpassword(request):
